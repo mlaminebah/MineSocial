1015a8a7d5834fc5e122242fd06c8eac9892b7d2
Mamadou Lamine BAH <mamadoulamine.bah@yahoo.fr>
2021-10-22 22:19:15 +0200

    Ajout de validation du premier formulaire

--- MineSocial/views.py
+++ MineSocial/views.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 
 #from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from datetime import datetime
 
 def welcome (request):
     return render (request,'welcome.html',{'current_date_time': datetime.now})
 def login (request):
-    return render (request,'login.html')
+    #verfie si le formulaire à été envoyé : il faut que la taille du tableau post soit différent de 0
+    if len(request.POST) > 0:
+        #on véririe si les paramètres attends sont transmis
+        if 'email' not in request.POST or 'password' not in request.POST:
+            error = "vous devez renseigner l'email et le mot de passe"
+            #return render (request,'login.html',{'error':error})
+        else:
+            email,paswd = request.POST['email'],request.POST['password']
+            #vérification des identifiants
+            if email != 'lamine@ici.fr' or paswd != 'lamine':
+                #si not ok
+                error = "Email ou mot de passe Invalide"
+                #return render (request,'login.html',{'error':error})
+            else:
+                #si ok -> page d'accueil
+                return redirect ('/welcome')
+        return render (request,'login.html',{'error':error})
+    else:
+        return render (request,'login.html')
